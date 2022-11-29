558cc4d3a5451346bac5ecceda42c89080c4f1e8
JinseokSong <me123alsl@gmail.com>
2022-11-30 01:56:56 +0900

    20221130 연관관계 맵핑

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -8,19 +8,30 @@ import javax.persistence.Persistence;
 public class JpaMain {
 
     public static void main(String[] args) {
-        // persistence.xml에 작성한 Persistence 명
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
         EntityManager em = emf.createEntityManager();
         EntityTransaction tx = em.getTransaction();
         tx.begin();
         try {
+            Team team = new Team();
+            team.setName("TeamA");
+            em.persist(team);
 
-            Member member1 = new Member();
-            member1.setName("member1");
-            System.out.println("==============================");
-            em.persist(member1);
-            System.out.println("member.id = " + member1.getId());
-            System.out.println("==============================");
+            Member member = new Member();
+            member.setUsername("member1");
+            em.persist(member);
+
+            team.addMember(member);
+
+            em.flush();
+            em.clear();
+
+            Member findMember = em.find(Member.class, member.getId());
+            Team findTeam = findMember.getTeam();
+
+            System.out.println("==================================");
+            System.out.println("==================================");
+
             tx.commit();
         }catch (Exception e){
             tx.rollback();
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -1,65 +1,30 @@
 package hellojpa;
 
 import javax.persistence.*;
-import java.util.Date;
 
 @Entity
-//@SequenceGenerator(
-//        name = "MEMBER_SEQ_GENERATOR",
-//        sequenceName = "MEMBER_SEQ", // 매핑할 데이터베이스 시퀀스 이름
-//        initialValue = 1, allocationSize = 50
-//)
-
-//@TableGenerator(
-//        name = "MEMBER_SEQ_GENERATOR",
-//        table = "MY_SEQUENCES",
-//        pkColumnValue = "MEMBER_SEQ", allocationSize = 1
-//)
 public class Member {
     @Id
-// //IDENTITY 전략
- @GeneratedValue(strategy = GenerationType.IDENTITY)
-
-//  //SEQUENCE 전략
-//  @GeneratedValue(strategy = GenerationType.SEQUENCE
-//  generator = "MEMBER_SEQ_GENERATOR")
-
-//    // TABLE 전략
-//    @GeneratedValue(strategy = GenerationType.TABLE,
-//            generator = "MEMBER_SEQ_GENERATOR")
+    @GeneratedValue
+    @Column(name = "MEMBER_ID")
     private Long id;
 
+    @Column(name = "USERNAME")
+    private String username;
 
-    @Column(name = "name", nullable = false)
-    private String name;
+//    @Column(name = "TEAM_ID")
+//    private Long teamId;
 
-    @Enumerated(EnumType.STRING)
-    private RoleType roleType;
+    @ManyToOne
+    @JoinColumn(name = "TEAM_ID")
+    private Team team;
 
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date createdDate;
-
-    @Temporal(TemporalType.TIMESTAMP )
-    private Date lastModifiedDate;
-
-    @Lob
-    private String description;
-
-    public Member() {
+    public String getUsername() {
+        return username;
     }
 
-    public Member(Long id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-
-    public Member(Long id, String name, RoleType roleType, Date createdDate, Date lastModifiedDate, String description) {
-        this.id = id;
-        this.name = name;
-        this.roleType = roleType;
-        this.createdDate = createdDate;
-        this.lastModifiedDate = lastModifiedDate;
-        this.description = description;
+    public void setUsername(String username) {
+        this.username = username;
     }
 
     public Long getId() {
@@ -70,43 +35,23 @@ public class Member {
         this.id = id;
     }
 
-    public String getName() {
-        return name;
+//    public Long getTeamId() {
+//        return teamId;
+//    }
+//
+//    public void setTeamId(Long teamId) {
+//        this.teamId = teamId;
+//    }
+
+
+    public Team getTeam() {
+        return team;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setTeam(Team team) {
+        this.team = team;
+        team.getMembers().add(this);
     }
 
-    public RoleType getRoleType() {
-        return roleType;
-    }
 
-    public void setRoleType(RoleType roleType) {
-        this.roleType = roleType;
-    }
-
-    public Date getCreatedDate() {
-        return createdDate;
-    }
-
-    public void setCreatedDate(Date createdDate) {
-        this.createdDate = createdDate;
-    }
-
-    public Date getLastModifiedDate() {
-        return lastModifiedDate;
-    }
-
-    public void setLastModifiedDate(Date lastModifiedDate) {
-        this.lastModifiedDate = lastModifiedDate;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
 }
